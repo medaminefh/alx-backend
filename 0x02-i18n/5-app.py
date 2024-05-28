5b30622f0aad0639f74fcc0e7fbbf1581c93dfac
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-05-28 11:04:58 +0000

    fix rendering user name

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -57,7 +57,7 @@ def before_request():
 @app.route('/')
 def index():
     """ index """
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
