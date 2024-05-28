864ec0231abc093f59819e7bea65acf84866a014
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-05-28 10:47:26 +0100

    i18n

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+""" Flask app """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ Config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale """
+    return request.accept_languages.best_match(['de', 'fr', 'en'])
+
+
+@app.route('/')
+def index():
+    """ index """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
